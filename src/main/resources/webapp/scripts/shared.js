596ec76f612a4e1217f76bb0cf7a401dea354f82
Andrei Brazhnikov <abrazhnikov@samokat.ru>
2025-12-01 09:45:00 +0100

    попытки улчшить положение лески

--- src/main/resources/webapp/scripts/shared.js
+++ src/main/resources/webapp/scripts/shared.js
@@ -443,7 +443,8 @@
     }
     return null;
   };
-  const ROD_TIP_ANCHOR_DEFAULT = { x: 0.07878, y: 0.04785 };
+  const ROD_TIP_ANCHOR_DEFAULT = { x: 0.07878, y: 0.0478 };
+//  const ROD_TIP_ANCHOR_DEFAULT = { x: 0.07878, y: 0.04785 };
   const ROD_CONFIG = {
     spark: {
       image: '/app/assets/rods/yellow_rod.png',
